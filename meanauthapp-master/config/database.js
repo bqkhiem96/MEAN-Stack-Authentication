92521e495393bb5f7b2b7ba5d2da897555036b55
UsbUnpluger <bqkhiem96@gmail.com>
2017-08-12 14:46:55 +0700

    Final commit

---
+++ meanauthapp-master/config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  database: 'mongodb://khiem:123456@ds129023.mlab.com:29023/meanauthapps',   //prod
+  // database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
